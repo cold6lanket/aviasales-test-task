de228d3ab422feb17131fc8c492bb96dd744f6ef
Feiertag <kz9merek@gmail.com>
2021-07-21 21:50:18 +0500

    added main logo

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import Tabs from './components/Tabs/Tabs';
 import ErrorBoundary from './components/ErrorBoundary';
 
 import Tickets from './components/Tickets/Tickets';
+import Logo from "./assets/Logo.svg";
 
 function App() {
 
-
-
     return (
         <div className="container">
+            <div className="start-logo">
+                <img src={Logo} alt="plane-logo" />
+            </div>
             <div className="main-content__inner">
                 <Filter />
                 <div className="tickets-section">
